18f2005f63be95789bb9f64e12d6e0db095e3e36
Gio <gpiantoni.work+github@gmail.com>
2012-02-19 17:56:58 +0100

    read and lmer in r

--- callr.m
+++ callr.m
@@ -1,7 +1,17 @@
-function lmer_dur_pow(cfg)
+function callr(cfg)
+%CALLR call subfunctions using R
+
 % call lmer to do lmm on dur and pow
 % model 
 % dur ~ pow subset(data, cond =='ns')
 % then
 % dur ~ pow * cond where interaction and main effect are not significant
+
+rdir = [cfg.scrp cfg.proj '_private/rfunctions/'];
+
+for i = 1:numel(cfg.callr.fun)
+  funname = [rdir cfg.callr.fun(i).name];
+  args = sprintf(' %s', cfg.callr.fun(i).arg{:}); % this should be more flexible
+  system(['Rscript ' funname ' ' args]);
+end
 
---
+++ rfunctions/preparedf.R
@@ -0,0 +1,20 @@
+#PREPAREDF: prepare dataframe for alpha dur
+
+#-----------------#
+#-pass arguments
+#1. data to import, csv
+#2. name of the file to save to
+args <- commandArgs(TRUE)
+#-----------------#
+
+#-----------------#
+#-read the data
+df <- read.csv(args[[1]], header=FALSE)
+colnames(df) <- c('subj', 'cond', 'trl', 'dur', 'elec', 'pow')
+
+df$subj <- factor(df$subj)
+df$trl <- factor(df$trl)
+df$powlog <- log(df$pow)
+
+save(df, file=args[[2]])
+#-----------------#
