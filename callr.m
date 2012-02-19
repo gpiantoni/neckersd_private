180d99625afc7457370da1a37caea88a8a15e6aa
Gio <gpiantoni.work+github@gmail.com>
2012-02-19 18:34:10 +0100

    topoplot tval from lmer

--- callr.m
+++ callr.m
@@ -1,17 +1,69 @@
 function callr(cfg)
 %CALLR call subfunctions using R
+% this cannot be more flexible, it depends on the subfunctions anyway
 
-% call lmer to do lmm on dur and pow
-% model 
-% dur ~ pow subset(data, cond =='ns')
-% then
-% dur ~ pow * cond where interaction and main effect are not significant
-
+%-----------------%
+%-common
 rdir = [cfg.scrp cfg.proj '_private/rfunctions/'];
+Rdata = [cfg.dpow 'dur_pow.Rdata'];
+elecbase = [cfg.dpow 'lmerelec'];
+load(cfg.sens.layout, 'layout')
+%-----------------%
 
-for i = 1:numel(cfg.callr.fun)
-  funname = [rdir cfg.callr.fun(i).name];
-  args = sprintf(' %s', cfg.callr.fun(i).arg{:}); % this should be more flexible
-  system(['Rscript ' funname ' ' args]);
+%-----------------%
+% 1. prepare df
+funname = [rdir 'preparedf.R'];
+args = [cfg.intor.csv ' ' Rdata];
+system(['Rscript ' funname ' ' args]);
+%-----------------%
+
+%-----------------%
+% 2. lmer on each electrodes and plot
+funname = [rdir 'lmer_elec.R'];
+args = [Rdata ' ' elecbase];
+system(['Rscript ' funname ' ' args]);
+eff = {'mainpow' 'maincond' 'powcond'};
+
+h = figure;
+for i = 1:numel(eff)
+  subplot(2,2,i)
+  plottvalue(eff{i}, layout, elecbase)
 end
 
+%--------%
+%-save and link
+pngname = sprintf('lmer_topo');
+saveas(h, [cfg.log filesep pngname '.png'])
+close(h); drawnow
+
+[~, logfile] = fileparts(cfg.log);
+system(['ln ' cfg.log filesep pngname '.png ' cfg.rslt pngname '_' logfile '.png']);
+%--------%
+%-----------------%
+
+
+% cfg.callr.fun(1).arg{1} = cfg.intor.csv;
+% cfg.callr.fun(1).arg{2} = Rdata;
+% 
+% cfg.callr.fun(2).name = 'lmer_dur_pow.R';
+% cfg.callr.fun(2).arg{1} = Rdata;
+% cfg.callr.fun(2).arg{2} = [cfg.dpow 'outputR.txt'];
+% cfg.callr.fun(2).arg{3} = 'E21';
+
+%-------------------------------------%
+%-subfunction read data and plot
+function plottvalue(cond, layout, elecbase)
+
+fid = fopen([elecbase cond '.csv'], 'r');
+C = textscan(fid, '%s %n');
+fclose(fid);
+
+[~, ilay, idat] = intersect(layout.label, upper(C{1}));
+
+ft_plot_lay(layout, 'label', 'no', 'point', 'no', 'box', 'no')
+hold on
+[~, h] = ft_plot_topo(layout.pos(ilay, 1), layout.pos(ilay, 2), C{2}(idat), 'mask', layout.mask);
+colorbar
+title(cond)
+set(get(h, 'parent'), 'clim', [-1 1] * 3)
+%-------------------------------------%
---
+++ rfunctions/lmer_elec.R
@@ -0,0 +1,37 @@
+#LMER_ELEC: lmer on each electrode 
+
+#-----------------#
+#-pass arguments
+#1. file of the dataset
+#2. elec file base
+args <- commandArgs(TRUE)
+#-----------------#
+
+#-----------------#
+library('lme4')
+load(args[[1]])
+
+mainpow <- numeric(0)
+maincond <- numeric(0)
+powcond <- numeric(0)
+
+for (e in levels(df$elec)){
+  print(e)
+  lm1 <- lmer(dur ~ powlog * cond + (1|subj), subset(df, elec==e))
+  mainpow[e] <- (summary(lm1))@coefs[2,3]
+  maincond[e] <- (summary(lm1))@coefs[3,3]
+  powcond[e] <- (summary(lm1))@coefs[4,3]
+}
+#-----------------#
+
+#-----------------#
+#-write to file
+csvelec <- paste(args[[2]], 'mainpow.csv', sep='')
+write.table(mainpow, file=csvelec, col.names=FALSE, quote=FALSE)
+
+csvelec <- paste(args[[2]], 'maincond.csv', sep='')
+write.table(maincond, file=csvelec, col.names=FALSE, quote=FALSE)
+
+csvelec <- paste(args[[2]], 'powcond.csv', sep='')
+write.table(powcond, file=csvelec, col.names=FALSE, quote=FALSE)
+#-----------------#
