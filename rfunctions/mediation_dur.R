23ec42d96e6849d1953997b34722bef3afce69e2
Gio <github@gpiantoni.com>
2012-10-08 17:04:17 +0200

    ENH: mediation works with normal input

--- rfunctions/mediation_dur.R
+++ rfunctions/mediation_dur.R
@@ -3,33 +3,48 @@
 #-----------------#
 #-pass arguments
 #1. file of the dataset
-#2. output file
-#3. electrode
+#2. 'pow', 'pow1', 'pow2', 'pow3', 'pow4'
+#3. 'sess' or 'nosess'
+#4. output file
 args <- commandArgs(TRUE)
 #-----------------#
 
 #-----------------#
+#-library
 library('lme4')
+#-----------------#
+
+#-----------------#
+#-data
 datfile <- args[[1]]
 load(datfile)
-sink(args[[2]], append=TRUE)
+outputfile <- args[[4]]
 
-print('XXX Alpha Mediation of Sleep Deprivation and Perceptual duration XXX')
-dfp <- subset(df, elec %in% eval(parse(text=args[[3]])))
+# get rid of confusing columns
+df$alphapow <- df[,args[[2]]]
+df <- df[,!(names(df) %in% c('pow', 'pow1', 'pow2', 'pow3', 'pow4'))]
 
-if (args[[4]] == 'sess') {
-  dfp <- aggregate(cbind(durlog, powlog, pow, day) ~ subj + cond + trl + sess, data = dfp, mean) # average over electrodes
+sink(outputfile, append=TRUE)
+cat('\n\n\nMEDIATION_DUR\n\n')
+
+if (args[[3]] == 'sess') {
+  dfp <- aggregate(cbind(alphapow, dur, day) ~ subj + cond + trl + sess, data = df, mean) # average over electrodes
 } else {
-  dfp <- aggregate(cbind(durlog, powlog, pow, day, sess) ~ subj + cond + trl, data = dfp, mean) # average over electrodes
+  dfp <- aggregate(cbind(alphapow, dur, day, sess) ~ subj + cond + trl, data = df, mean) # average over electrodes
 }
 
 #aggregate transforms them into numberic again
 dfp$day <- factor(dfp$day)
 dfp$sess <- ordered(dfp$sess)
 
-model1 <- lmer(durlog ~ cond + (1|subj) + (1|day:subj) + (1|sess:day:subj), data = dfp)
-model2 <- lmer(durlog ~ cond + powlog + (1|subj) + (1|day:subj) + (1|sess:day:subj), data = dfp)
-model3 <- lmer(powlog ~ cond + (1|subj) + (1|day:subj) + (1|sess:day:subj), data = dfp)
+summary(dfp)
+#-----------------#
+
+#-----------------#
+#-run models
+model1 <- lmer(dur ~ cond + (1|subj) + (1|day:subj) + (1|sess:day:subj), data = dfp)
+model2 <- lmer(dur ~ cond + alphapow + (1|subj) + (1|day:subj) + (1|sess:day:subj), data = dfp)
+model3 <- lmer(alphapow ~ cond + (1|subj) + (1|day:subj) + (1|sess:day:subj), data = dfp)
 
 formula(model1)
 print(summary(model1)@coefs)
@@ -37,7 +52,10 @@ formula(model2)
 print(summary(model2)@coefs)
 formula(model3)
 print(summary(model3)@coefs)
+#-----------------#
 
+#-----------------#
+#-collect and print info
 r.1 <- summary(model1)@coefs[2,1]
 se.1 <- summary(model1)@coefs[2,2]
 
@@ -76,8 +94,12 @@ print('zvalue')
 print(zvalue)
 print('p-value')
 print(2*(1-pnorm(abs(zvalue))))
-sink()
 
-infofile <- paste(substr(datfile, 1, nchar(datfile)-6), 'csv', sep='.')
+sink()
+#-----------------#
+
+#-----------------#
+#-print to file
+infofile <- paste(substr(outputfile, 1, nchar(outputfile)-12), 'output_mediation', '.csv', sep='')
 write.table(zvalue, file=infofile, row.names=FALSE, col.names=FALSE, quote=FALSE, append=TRUE)
 #-----------------#
