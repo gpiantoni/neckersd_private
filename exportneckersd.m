5c0f463320fac64876b0de241325559a921eb444
Gio <github@gpiantoni.com>
2012-10-08 18:26:25 +0200

    ENH: interesting output

--- exportneckersd.m
+++ exportneckersd.m
@@ -1,62 +1,64 @@
-function output = exportneckersd(cfg)
-%EXPORTNECKERSD add extra columns to export2csv
-
-output = 'LMER,';
+function exportneckersd(info, cfg)
+%EXPORTNECKERSD export info from neckersd
 
 %-------------------------------------%
-%-redef
-output = [output sprintf('%s,', cfg.redef.event2trl)];
-output = [output sprintf('%s,', cfg.redef.trigger)];
-output = [output sprintf('%f,', cfg.redef.mindist)];
-output = [output sprintf('%f,', cfg.redef.maxdist)];
+%-general
+[~, logname] = fileparts(info.log);
+output = [logname ','];
 
-if strcmp(cfg.redef.event2trl, 'event2trl_trial')
-  output = [output sprintf('%f,', cfg.redef.prestim)];
-  output = [output sprintf('%f,', cfg.redef.poststim)];
-  output = [output sprintf('%f,', cfg.intor.powpeak(1).time)];
-  output = [output sprintf('%f,', cfg.intor.powpeak(1).wndw)];
-  
-else
-  output = [output sprintf('%f,', cfg.redef.pad)];
-  output = [output sprintf('%f,', cfg.redef.trldur)];
-  output = [output sprintf('%f,', cfg.redef.overlap)];
-  
+for i = 1:numel(info.run)
+  output = [output sprintf('%d %s ', info.run(i), cfg(info.run(i)).function)];
 end
 %-------------------------------------%
 
 %-------------------------------------%
-%-POW info
-output = [output sprintf('%f,', cfg.intor.powpeak.freq(1))];
-output = [output sprintf('%f,', cfg.intor.powpeak.freq(2))];
-%-------------------------------------%
-
-%-------------------------------------%
-%-stats
-output = [output sprintf('%f,', numel(cfg.callr.selelec))];
+%-from cfg to log
+output = [output struct2log(cfg, 'csv')];
 %-------------------------------------%
 
 %-------------------------------------%
 %-read results
-extradata = [cfg.dcor 'dur_pow.csv'];
-lmerinfo = dlmread(extradata);
-output = [output  sprintf('%f,', lmerinfo)];
-
+csvinfo = {'main' 'predict' 'mediation' 'singlesubj'};
+for i = 1:numel(csvinfo)
+  csvfile = [info.log filesep 'output_' csvinfo{i} '.csv'];
+  
+  if exist(csvfile, 'file')
+    output = [output csvinfo{i} ','];
+    
+    lmerinfo = dlmread(csvfile);
+    output = [output  sprintf('%f,', lmerinfo)];
+  end
+  
+end
 %-----------------%
 %-prepare output
-% columns contain:
+% MAIN
 % 1- lmer powlog only ns
 % 2- lmer powlog only sd
 % 3- lmer powlog X cond: powlog
 % 4- lmer powlog X cond: cond
 % 5- lmer powlog X cond: interaction
-% 6- lmer mediation
+ 
+% PREDICT
+% 1- most significant value
+% 2- index of the most significant value
+% 3- # of significant values at 0.05
+% 4- # of significant values at 0.1
+
+% MEDIATION
+% 1- z-value for mediation
+
+% SINGLE-SUBJ
+% 1- p-value for alpha
+% 2- p-value for perceptual duration
+% 3- p-value for correlation
 %-----------------%
 %-------------------------------------%
 
 %-------------------------------------%
-%-clean up files
-delete(extradata)
-% delete([extradata(1:end-3) 'Rdata'])
-% delete([cfg.dcor 'r_powpeak.mat'])
-delete([cfg.dcor 'lmerelec*']);
+%-write to file
+fid = fopen(cfg(end).opt.csvf, 'a+');
+fwrite(fid, output);
+fprintf(fid, '\n');
+fclose(fid);
 %-------------------------------------%
